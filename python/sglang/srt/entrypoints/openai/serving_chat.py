b545c6895f4e172662762d340f7dac67442edb3f
TideDra <gearyzhang@outlook.com>
2026-03-04 23:09:13 +0800

    support router http mode (Python part only)

--- python/sglang/srt/entrypoints/openai/serving_chat.py
+++ python/sglang/srt/entrypoints/openai/serving_chat.py
@@ -385,6 +385,12 @@ class OpenAIServingChat(OpenAIServingBase):
                     raise ValueError(
                         "When using trajectory tracking, the prompt_ids must be a list"
                     )
+                # Preprocess the case where cached traj is not deleted after the first response.
+                if request.traj_id in self.traj_map and request.trajectory:
+                    maybe_updated_traj = Trajectory.model_validate(request.trajectory)
+                    if maybe_updated_traj.cached_token_ids:
+                        del self.traj_map[request.traj_id]
+
                 if request.traj_id not in self.traj_map and not request.trajectory:
                     # Neither cached traj nor given traj. Internally initialize a new traj.
                     cached_token_ids = result.prompt_ids
@@ -401,7 +407,7 @@ class OpenAIServingChat(OpenAIServingBase):
                 elif request.traj_id in self.traj_map and request.trajectory:
                     raise ValueError(
                         f"traj with id {request.traj_id} has been cached, "
-                        f"but traj is given in the request"
+                        f"but an empty traj is given in the request"
                     )
                 else:
                     # Either cached traj or given traj.
@@ -1121,6 +1127,7 @@ class OpenAIServingChat(OpenAIServingBase):
                 raise ValueError(
                     f"Unexpected error: traj with id {request.traj_id} is not found"
                 )
+            traj.cached_request.trajectory = None
             metadata["trajectory"] = traj.model_dump()
 
         return ChatCompletionResponse(
@@ -1129,7 +1136,7 @@ class OpenAIServingChat(OpenAIServingBase):
             model=request.model,
             choices=choices,
             usage=usage,
-            metadata={"weight_version": ret[0]["meta_info"]["weight_version"]},
+            metadata=metadata,
             sglext=response_sglext,
         )
 
