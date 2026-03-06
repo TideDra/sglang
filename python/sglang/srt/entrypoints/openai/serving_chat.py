d073f94801c6d7b15d0eb773a61e2ec4f9fe905f
idawnlight <idawn@live.com>
2026-03-06 23:22:50 +0800

    fix: duplicated return_routed_experts

--- python/sglang/srt/entrypoints/openai/serving_chat.py
+++ python/sglang/srt/entrypoints/openai/serving_chat.py
@@ -332,7 +332,6 @@ class OpenAIServingChat(OpenAIServingBase):
             image_max_dynamic_patch=img_max_dynamic_patch,
             video_max_dynamic_patch=vid_max_dynamic_patch,
             max_dynamic_patch=getattr(request, "max_dynamic_patch", None),
-            return_routed_experts=request.return_routed_experts,
         )
 
         return adapted_request, request
